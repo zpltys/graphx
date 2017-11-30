84a383605d63e256cbae6812e4a07a6855754807
zpltys <gzpltys@gmail.com>
2017-11-30 19:59:46 +0800

    test

--- src/main/scala/GraphSim.scala
+++ src/main/scala/GraphSim.scala
@@ -41,7 +41,7 @@ object GraphSim {
     val startTime = System.currentTimeMillis()
 
     val partition = args(0).toInt
- /*
+
     val vertex = sc.textFile("alluxio://hadoopmaster:19998/zpltys/graphData/label.txt", minPartitions = 10).flatMap(line => {
       val msg = line.split('\t')
       val id = msg(0).toLong
@@ -50,7 +50,7 @@ object GraphSim {
         Some((id, (label, mutable.Set[VertexId]())))
       } else None
     }).cache()
-   */
+   /*
 
     val vertex = sc.textFile("alluxio://hadoopmaster:19998/zpltys/graphData/label.txt", minPartitions = partition * 4).map(line => {
       val msg = line.split('\t')
@@ -59,7 +59,7 @@ object GraphSim {
 
       (id, (label, mutable.Set[VertexId]()))
     }).cache()
-
+*/
 
     println("zs-log: vertex.size:" + vertex.count())
 
@@ -71,11 +71,11 @@ object GraphSim {
       (u, v)
     })
 
-   // val filterU = tmpPair.join(vertex).map(tuple => (tuple._2._1, tuple._1))
-   // val filterV = filterU.join(vertex).map(tuple => (tuple._2._1, tuple._1))
-   // val edge = filterV.map(e => Edge(e._1, e._2, 0))
+    val filterU = tmpPair.join(vertex).map(tuple => (tuple._2._1, tuple._1))
+    val filterV = filterU.join(vertex).map(tuple => (tuple._2._1, tuple._1))
+    val edge = filterV.map(e => Edge(e._1, e._2, 0)).cache()
 
-    val edge = tmpPair.map(edge => Edge(edge._1, edge._2, 0)).cache()
+    //val edge = tmpPair.map(edge => Edge(edge._1, edge._2, 0)).cache()
 
     println("zs-log: edge.size:" + edge.count())
 
@@ -113,10 +113,6 @@ object GraphSim {
     postGraph.unpersist()
     graph.unpersist()
 
-
-    println("zs-log: tempG.vertices:" + tempG.vertices.count())
-
-
     val postCount = tempG.triplets.map(triplets => {
       (triplets.srcId, triplets.dstAttr)
     }).reduceByKey((a, b) => {
@@ -146,10 +142,11 @@ object GraphSim {
         }
       }
       (attr._1, deleteSet, message)
-    })
+    }).cache()
 
     tempG.unpersist()
 
+    println("zs-log: initial count: " + initialGraph.vertices.count())
     val initialTime = System.currentTimeMillis()
     println("zs-log: finish initial, initial time: " + (initialTime - loadOk) / 1000 + "s")
 
@@ -186,6 +183,8 @@ object GraphSim {
         a
       })
 
+    initialGraph.unpersist()
+
     println("zs-log: final graph calculated, vertex size:" + finalGraph.vertices.count())
     val stopTime = System.currentTimeMillis()
     println("zs-log: finish calculated, iteration time:" + (stopTime - initialTime) / 1000 + "s")
@@ -196,7 +195,7 @@ object GraphSim {
         buffer.append((s, v._1))
       }
       buffer
-    }).saveAsTextFile("alluxio://hadoopmaster:19998/zpltys/graphData/sim" + partition)
+    }).saveAsTextFile("alluxio://hadoopmaster:19998/zpltys/graphData/sim" + partition + "_" + args(1))
 
    sc.stop()
   }
